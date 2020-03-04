452df184ace15de288920382ce852795be55a502
xavi <xabi1309@gmail.com>
2020-03-04 14:02:37 +0100

    Adding schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_03_130740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shorteners", force: :cascade do |t|
+    t.text "original_url"
+    t.string "shorten_url"
+    t.string "sanitized_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
